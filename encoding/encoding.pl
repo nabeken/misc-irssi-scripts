4d87fe4d9d3347f3eb9dd7e4a7a84c39bc05a943
TANABE Ken-ichi <nabeken@tknetworks.org>
2008-04-03 04:57:59 +0900

    Support: message_own_public, message_public

--- encoding/encoding.pl
+++ encoding/encoding.pl
@@ -1,10 +1,8 @@
-#!/usr/bin/perl
-
 # based on conv.pl
 # http://www.binzume.net/library/irssi.html
 
-use Irssi;
 use Encode;
+use Irssi;
 
 our $VERSION = '0.03';
 our %IRSSI   = (
@@ -18,16 +16,16 @@ our %IRSSI   = (
 
 my $local_charset = 'UTF-8';
 
-my $channelsettings = (
+my %channelsettings = (
   '#gentoo-ja' => 'UTF-8',
-  '#testfornabeken' => 'ISO-2022-JP';
+  '#testfornabeken' => 'ISO-2022-JP',
 );
 
 sub send_text {
     my ( $text, $server, $witem ) = @_;
 
     if ($server && $witem) {
-        my $target_charset = $channelsettings{$witem->Channnel->{name}}
+        my $target_charset = $channelsettings{$witem->{name}};
 	Encode::from_to($text, $local_charset, $target_charset) if defined $target_charset;
     }
 
@@ -38,43 +36,27 @@ Irssi::signal_add( 'send text', 'send_text' );
 
 sub send_command {
     my ($command,$server,$item) = @_;
-    my $target_charset = $channelsettings{$witem->Channnel->{name}}
+    my $target_charset = $channelsettings{$item->{name}};
     Encode::from_to($text, $local_charset, $target_charset) if defined $target_charset;
     Irssi::signal_continue( $command, $server, $item );
 }
 
 Irssi::signal_add( 'send command', 'send_command' );
 
-sub print_text {
-    my ( $dest, $text, $stripped ) = @_;
-      if ($text!~/Topic for .+:/) {
-        my $target_charset = $channelsettings{$witem->Channnel->{name}}
-	Encode::from_to($text, $local_charset, $target_charset) if defined $target_charset;
-      }
-    Irssi::signal_continue( $dest, $text, $stripped );
+sub message_public {
+    my ($server, $msg, $nick, $addr, $target) = @_;
+    my $target_charset = $channelsettings{$target};
+    Encode::from_to($msg, $target_charset, $local_charset);
+    Irssi::signal_continue($server, $msg, $nick, $addr, $target);
 }
 
-Irssi::signal_add( 'print text', 'print_text' );
+Irssi::signal_add('message public', 'message_public');
 
-sub message_topic {
-    my ( $server, $chan, $topic, $nick, $addr ) = @_;
-    my $target_charset = $channelsettings{$witem->Channnel->{name}}
-    Encode::from_to($text, $local_charset, $target_charset) if defined $target_charset;
-    Irssi::signal_continue( $server, $chan, $topic, $nick, $addr );
+sub message_own_public {
+    my ($server, $msg, $target) = @_;
+    my $target_charset = $channelsettings{$target};
+    Encode::from_to($msg, $target_charset, $local_charset);
+    Irssi::signal_continue($server, $msg, $target);
 }
 
-Irssi::signal_add_first( 'message topic', 'message_topic' );
-
-sub event_topic {
-    my ($server, $data, $nick, $address) = @_;
-    my ($channel, $topic) = split(/ :/, $data, 2);
-    my $target_charset = $channelsettings{$witem->Channnel->{name}}
-    Encode::from_to($text, $local_charset, $target_charset) if defined $target_charset;
-    $data = "$channel :$topic";
-
-    Irssi::signal_continue( $server, $data, $nick, $address );
-}
-
-Irssi::signal_add_first("event topic", "event_topic");
-Irssi::signal_add_first("event 332", "event_topic");
-Irssi::signal_add_first("event 333", "event_topic");
+Irssi::signal_add('message own_public', 'message_own_public');
