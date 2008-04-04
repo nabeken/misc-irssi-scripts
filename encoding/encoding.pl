e97cec532dbc5d72c0d6d339a74ce8f2d85dc56c
TANABE Ken-ichi <nabeken@tknetworks.org>
2008-04-05 04:42:00 +0900

    Add /join for not utf8 channel names support
    but does not support not utf8 topic.

--- encoding/encoding.pl
+++ encoding/encoding.pl
@@ -20,31 +20,57 @@ my $local_charset = 'UTF-8';
 my %channelsettings = (
     '#gentoo-ja' => 'UTF-8',
     '#testfornabeken' => 'ISO-2022-JP',
+    '#なべけんてすと' => 'ISO-2022-JP',
 );
 
+my %channelsettings_reverse;
+
+foreach $key (keys(%channelsettings)) {
+    $target_charset = $channelsettings{$key};
+    Encode::from_to($key, $local_charset, $target_charset);
+    $channelsettings_reverse{$key} = $target_charset;
+}
+
 sub send_text {
-    my ( $text, $server, $witem ) = @_;
+    my ($text, $server, $witem) = @_;
 
     if ($server && $witem) {
         my $target_charset = $channelsettings{$witem->{name}};
         Encode::from_to($text, $local_charset, $target_charset) if defined $target_charset;
     }
 
-    Irssi::signal_continue( $text, $server, $witem );
+    Irssi::signal_continue($text, $server, $witem);
 }
-Irssi::signal_add( 'send text', 'send_text' );
+Irssi::signal_add('send text', 'send_text');
 
 sub send_command {
-    my ($command,$server,$item) = @_;
-    my $target_charset = $channelsettings{$item->{name}};
-    Encode::from_to($text, $local_charset, $target_charset) if defined $target_charset;
-    Irssi::signal_continue( $command, $server, $item );
+    my ($command, $server, $item) = @_;
+    my $target_charset;
+
+    if ($command =~ /^\/join (.*)/) {
+	my $chan = $1;
+	$target_charset = $channelsettings{$chan};
+    } else {
+	my $char = $item->{name};
+	$target_charset = $channelsettings_reverse{$item->{name}};
+    }
+
+    Encode::from_to($command, $local_charset, $target_charset) if defined $target_charset;
+    Irssi::signal_continue($command, $server, $item);
 }
-Irssi::signal_add( 'send command', 'send_command' );
+Irssi::signal_add('send command', 'send_command');
+
+sub channel_created {
+    my ($chan, $auto) = @_;
+    my $target_charset = $channelsettings_reverse{$chan->{name}};
+    Encode::from_to($chan->{name}, $target_charset, $local_charset);
+    Irssi::signal_continue($chan, $auto);
+}
+Irssi::signal_add('channel created', 'channel_created');
 
 sub message_public {
     my ($server, $msg, $nick, $addr, $target) = @_;
-    my $target_charset = $channelsettings{$target};
+    my $target_charset = $channelsettings_reverse{$target};
     Encode::from_to($msg, $target_charset, $local_charset) if defined $target_charset;
     Irssi::signal_continue($server, $msg, $nick, $addr, $target);
 }
@@ -52,7 +78,7 @@ Irssi::signal_add('message public', 'message_public');
 
 sub message_own_public {
     my ($server, $msg, $target) = @_;
-    my $target_charset = $channelsettings{$target};
+    my $target_charset = $channelsettings_reverse{$target};
     Encode::from_to($msg, $target_charset, $local_charset) if defined $target_charset;
     Irssi::signal_continue($server, $msg, $target);
 }
@@ -60,7 +86,7 @@ Irssi::signal_add('message own_public', 'message_own_public');
 
 sub message_irc_action {
     my ($server, $msg, $nick, $addr, $target) = @_;
-    my $target_charset = $channelsettings{$target};
+    my $target_charset = $channelsettings_reverse{$target};
     Encode::from_to($msg, $target_charset, $local_charset) if defined $target_charset;
     Irssi::signal_continue($server, $msg, $nick, $addr, $target);
 }
@@ -68,9 +94,10 @@ Irssi::signal_add('message irc action', 'message_irc_action');
 
 sub message_topic {
     my ($server, $chan, $topic, $nick, $addr) = @_;
-    my $target_charset = $channelsettings{$chan};
+    my $target_charset = $channelsettings_reverse{$chan};
     Encode::from_to($topic, $target_charset, $local_charset) if defined $target_charset;
     Irssi::signal_continue($server, $chan, $topic, $nick, $addr);
 }
 Irssi::signal_add('message topic', 'message_topic');
+
 # vim: set ts=8 sw=4:
